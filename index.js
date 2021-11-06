8cd5e8cf6761ccf799f3960066c30988d81200e2
Aleksandar Alimpic <alimpicaleksandar@Aleksandars-MacBook-Air.local>
2021-11-06 17:49:29 +0100

    Add mongoDb

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
